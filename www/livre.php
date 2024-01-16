f797975f5c832f6fae738d4f970d9fc28f14f598
unknown <kronnetv83390@gmail.com>
2024-01-16 14:22:25 +0100

    update

--- www/livre.php
+++ www/livre.php
@@ -1,44 +1,37 @@
 <?php
-require_once "livre.class.php";
-$l1 = new Livre(1, "Apprendre le CSS", "apprendre-css.png", 200);
-$l2 = new Livre(2, "Apprendre Docker", "apprendre-docker.png", 400);
-$l3 = new Livre(3, "Apprendre le Java", "apprendre-java.png", 600);
-$l4 = new Livre(4, "Apprendre le JavScript", "apprendre-JS.png", 300);
-$l5 = new Livre(5, "Apprendre le PHP", "apprendre-php.png", 1000);
-$l6 = new Livre(3, "Apprendre le Wordpress", "apprendre-wordpress.png", 10);
-
-// $livres = [$l1, $l2, $l3, $l4, $l5, $l6];
 require_once "Livre.Manager.class.php";
+require_once "Livre.class.php";
+
 $livreManager = new LivreManager;
-$livreManager->AjouterLivre($l1);
-$livreManager->AjouterLivre($l2);
-$livreManager->AjouterLivre($l3);
+$livreManager->chargementLivres();
 
 ?>
+
 <?php ob_start() ?>
 
-<table class="table table-hover table-info">
-    <tr>
+<table class="table table-center">
+    <tr class="table-dark">
         <th>Image</th>
         <th>Titre</th>
         <th>Nombre de pages</th>
         <th colspan="2">Actions</th>
     </tr>
     <?php
-    foreach ($livres as $livre) : ?>
-        <tr class="table-secondary">
-            <td class="allign-middle"><img src="public/images/<?php echo $livre->getImage(); ?>" height="150px"></td>
-            <td class="allign-middle"><?php echo $livre->getTitre(); ?></td>
-            <td class="allign-middle"><?php echo $livre->getNbDePages(); ?></td>
-            <td class="allign-middle"><a href="#" class="btn btn-warning">Modifier</a></td>
-            <td class="allign-middle"><a href="#" class="btn btn-danger">Supprimer</a></td>
+    $livresEnCours = $livreManager->getLivres();
+
+    foreach ($livresEnCours as $livre) : ?>
+        <tr>
+            <td class="align-middle"><img src="public/images/<?php echo $livre->getImage(); ?>" height="150px" alt="Livre pour <?php echo $livre->getTitre(); ?>"></td>
+            <td class="align-middle"><?php echo $livre->getTitre(); ?></td>
+            <td class="align-middle"><?php echo $livre->getNbDePages(); ?></td>
+            <td class="align-middle"><a href="#" class="btn btn-warning">Modifier</a></td>
+            <td class="align-middle"><a href="#" class="btn btn-danger">Supprimer</a></td>
         </tr>
     <?php endforeach; ?>
 </table>
-<a href="#" class="btn btn-success d-block">Ajouter</a>
 
-
+<a href="#" class="btn btn-success d-block">Ajouter un livre</a>
 <?php
-$titre = "Ceci sont mes livre Hi Hi Hi";
+$titre = "Ceci est ma bibliothèque :D";
 $content = ob_get_clean();
 require_once "template.php";
